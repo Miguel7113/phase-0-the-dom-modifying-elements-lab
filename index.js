9d829db06b0032b3e25e7866464b3fbf26cf72b5
Miguel7113 <miguelndakwe11@gmail.com>
2024-07-11 20:09:27 +0300

    dom modifying

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const node = document.getElementById("main")
+if (node.parentNode) {
+    node.parentNode.removeChild(main);
+}
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = "is the champion";
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+console.log(newHeader.tagName);
